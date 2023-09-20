d77b7ba358bc3367346761b21c8de06f7b11d36b
Toba Ojo <tojo@axxess.com>
2023-09-20 18:03:27 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [randomUsers, setRandomUsers] = useState(null);
+  
+  const shortDateTime = (date) => {
+    let dateObject = new Date(date);
+    return dateObject.toLocaleDateString("en-GB");
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get('https://randomuser.me/api/')
+        .then((res) => {
+          setRandomUsers(res.data?.results);
+        });
+    }, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+
+  }, []);
+
+  return <>
+     {(randomUsers?.map((user,index)=>{
+       return(
+        <div className="card marginTop" key={index}>
+          <img src={user.picture.large} alt={`${user?.name}`}/>
+          <h3>{user.name.title} {user.name.first} {user.name.last}</h3>
+          <p style={{textTransform: "capitalize"}} >{user.gender}  ( {user.dob.age} )</p>
+          <p><strong>DOB: </strong>{shortDateTime(user.dob.date)}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+          <p><strong>Phone: </strong> {user.phone}</p>   
+        </div>
+       )
+     }))}
+  </>
 }
 
 export default App;
